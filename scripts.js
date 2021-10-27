740785122278b8af4cc830eb27efdb37f58fc37f
hytx <r.dinisnaruto@gmail.com>
2021-10-27 13:26:43 +0100

    autocomplete off, js esta com as funcs basicas, css mt rudimentar

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function cavidades(x){
+	alert("Nº cavidades = " + x);
+}
+function sementes(){
+	alert("Nº sementes = " + document.getElementById('sementes').value);
+}
+function pc(x){
+	alert("Jogar contra pc? " + document.getElementById('pc').value);
+}
+function dificuldade(x){
+	alert("Dificuldade: " + document.getElementById('dificuldade').value);
+}
